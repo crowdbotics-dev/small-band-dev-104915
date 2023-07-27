7f4681ece96c64d5989748dea8d1885022bf6df8
Crowdbotics <engineering@crowdbotics.com>
2023-07-27 23:18:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-band-dev-104915",
+  url: "https://small-band-dev-104915.botics.co",
+  api: "https://small-band-dev-104915.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
